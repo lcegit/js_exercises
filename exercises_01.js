aef1a297e2312fb7c9700eb24d96be0a84e31706
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-10-23 18:36:26 +0200

    exercise #1

---
+++ exercises_01.js
@@ -0,0 +1,18 @@
+var newArray = [1, 5, 3.22, 5, 54.3, 42.e4, 53.9743]
+
+numbers[3]
+
+myArray = ['Thomas', 'Amber', 'Raoul']
+emptyArray = []
+for(var n = 0; n < myArray.length; n++) {
+  emptyArray.push(myArray[n]);
+}
+
+
+var myVar = "Lisa";
+
+if (myVar === "Lisa") {
+    console.log("That's my name");
+} else {
+   console.log("That's not me");
+}
